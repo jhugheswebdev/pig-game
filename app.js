e3e0009eb9a46d95491af36965b5abaf282e41be
James Hughes <heezmagnif@gmail.com>
2018-04-26 10:44:55 -0400

    Changes winning score

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= 50) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
